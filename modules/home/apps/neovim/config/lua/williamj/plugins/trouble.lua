862f5b6e3ffc2e368182eddb650c74efb29c5e4f
William Jelgren <william@jelgren.dk>
2024-05-22 23:14:27 +0200

    Added <leader>xx to open Trouble pane

--- modules/home/apps/neovim/config/lua/williamj/plugins/trouble.lua
+++ modules/home/apps/neovim/config/lua/williamj/plugins/trouble.lua
@@ -1,10 +1,9 @@
 return {
   "folke/trouble.nvim",
-  branch = "dev",
   dependencies = {
     "nvim-tree/nvim-web-devicons"
   },
   keys = {
-    { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+    { "<leader>xx", function() require("trouble").toggle() end, desc = "Diagnostics (Trouble)" },
   },
 }
