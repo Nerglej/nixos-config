d3b1de0decf9b63fd2edd4bbdd5869edbb770eb8
William Jelgren <william@jelgren.dk>
2025-03-20 23:48:34 +0100

    Removed lsp-zero and used direct lsp config instead

--- modules/home/neovim/config/lua/williamj/plugins/lsp.lua
+++ modules/home/neovim/config/lua/williamj/plugins/lsp.lua
@@ -1,44 +1,83 @@
 return {
-  {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v3.x",
-    dependencies = {
-      -- LSP
-      {"neovim/nvim-lspconfig"},
+    { 'hrsh7th/cmp-nvim-lsp' },
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            { 'hrsh7th/cmp-nvim-lsp' },
+        },
+        config = function()
+            local cmp = require('cmp')
 
-      -- Auto-completion
-      {"hrsh7th/nvim-cmp"},
-      {"hrsh7th/cmp-nvim-lsp"},
-
-      -- Snippets
-      {"L3MON4D3/LuaSnip"},
-      {"rafamadriz/friendly-snippets"},
-      {"nvim-java/nvim-java"}
+            cmp.setup({
+                sources = {
+                    { name = 'nvim_lsp' },
+                },
+                snippet = {
+                    expand = function(args)
+                        -- You need Neovim v0.10 to use vim.snippet
+                        vim.snippet.expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({}),
+            })
+        end
     },
-    config = function()
-      local lsp_zero = require('lsp-zero')
-      lsp_zero.on_attach(function(client, bufnr)
-        lsp_zero.default_keymaps({buffer = bufnr})
-      end)
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            -- Auto-completion
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-nvim-lsp" },
 
-      local lspconfig = require('lspconfig')
+            -- Snippets
+            { "L3MON4D3/LuaSnip" },
+            { "rafamadriz/friendly-snippets" },
+            { "nvim-java/nvim-java" }
+        },
+        config = function()
+            local lspconfig_defaults = require('lspconfig').util.default_config
+            lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+                'force',
+                lspconfig_defaults.capabilities,
+                require('cmp_nvim_lsp').default_capabilities()
+            )
 
-      lspconfig.lua_ls.setup({})
-      lspconfig.nil_ls.setup({})
+            vim.api.nvim_create_autocmd('LspAttach', {
+                desc = 'LSP actions',
+                callback = function(event)
+                    local opts = { buffer = event.buf }
 
-      lspconfig.rust_analyzer.setup({})
-      lspconfig.csharp_ls.setup({})
-      lspconfig.jdtls.setup({})
+                    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+                    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+                    vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
+                    vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation() end, opts)
+                    vim.keymap.set('n', 'go', function() vim.lsp.buf.type_definition() end, opts)
+                    vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, opts)
+                    vim.keymap.set('n', 'gs', function() vim.lsp.buf.signature_help() end, opts)
+                    vim.keymap.set('n', '<F2>', function() vim.lsp.buf.rename() end, opts)
+                    vim.keymap.set({ 'n', 'x' }, 'gq', function() vim.lsp.buf.format({ async = true }) end, opts)
+                    vim.keymap.set('n', '<F4>', function() vim.lsp.buf.code_action() end, opts)
+                end,
+            })
 
-      -- Javascript I guess
-      -- lspconfig.tsserver.setup({})
-      -- lspconfig.svelte.setup({})
+            local lspconfig = require('lspconfig')
 
-      -- VSCode lsp's
-      lspconfig.eslint.setup({})
-      lspconfig.html.setup({})
-      lspconfig.cssls.setup({})
-      lspconfig.jsonls.setup({})
-    end
-  },
+            lspconfig.lua_ls.setup({})
+            lspconfig.nil_ls.setup({})
+
+            lspconfig.rust_analyzer.setup({})
+            lspconfig.csharp_ls.setup({})
+            lspconfig.jdtls.setup({})
+
+            -- Javascript I guess
+            -- lspconfig.tsserver.setup({})
+            -- lspconfig.svelte.setup({})
+
+            -- VSCode lsp's
+            lspconfig.eslint.setup({})
+            lspconfig.html.setup({})
+            lspconfig.cssls.setup({})
+            lspconfig.jsonls.setup({})
+        end
+    },
 }
