c51995fc76efaca1ca8d0ef5140019bb9de768c8
William Jelgren <william@jelgren.dk>
2025-05-27 21:55:00 +0200

    Cleaned up emporer config

--- nixos/emperor/default.nix
+++ nixos/emperor/default.nix
@@ -18,50 +18,81 @@ in {
   ];
 
   # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.overlays = [outputs.overlays.unstable-packages];
+  nixpkgs = {
+    config.allowUnfree = true;
+    overlays = [outputs.overlays.unstable-packages];
+  };
 
-  nix.package = pkgs.nixVersions.stable;
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-  nix.channel.enable = false;
+  nix = {
+    package = pkgs.nixVersions.stable;
+    settings.experimental-features = ["nix-command" "flakes"];
+    channel.enable = false;
+  };
 
   # Locale
   time.timeZone = systemSettings.timezone;
   i18n.defaultLocale = systemSettings.locale;
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # Networking
-  networking.hostName = systemSettings.hostname;
-  networking.networkmanager.enable = true;
-
-  # Firewall
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [
-      22 # OpenSSH
-      57621 # Spotify
-    ];
-    allowedUDPPorts = [
-      5353 # Spotify
-      4242 # lan-mouse
-    ];
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
   };
 
-  # User account
-  users.users = {
-    "williamj" = {
-      isNormalUser = true;
-      description = "William Jelgren";
-      extraGroups = [
-        "networkmanager"
-        "wheel"
-        "docker"
-        "libvirtd"
-      ];
-      uid = 1000;
+  hardware = {
+    bluetooth.enable = true;
+    bluetooth.powerOnBoot = true;
+
+    graphics = {
+      enable = true;
+      # For Steam
+      enable32Bit = true;
+
+      # nvidia driver
+      extraPackages = [pkgs.nvidia-vaapi-driver];
+    };
+
+    # Use open source nvidia drivers
+    nvidia.open = true;
+  };
+
+  services = {
+    timesyncd.enable = true;
+
+    # Enable the X11 windowing system.
+    xserver = {
+      enable = true;
+
+      # Configure keymap in X11
+      xkb.layout = "us";
+
+      # Set nvidia driver
+      videoDrivers = ["nvidia"];
+    };
+
+    # KDE Plasma
+    displayManager.sddm.enable = true;
+    desktopManager.plasma6.enable = true;
+
+    # OpenSSH
+    openssh = {
+      enable = true;
+      ports = [22];
+      settings = {
+        PasswordAuthentication = true;
+        AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
+        UseDns = true;
+        X11Forwarding = true;
+        PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
+      };
+    };
+
+    # Enable pipewire for sound
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
     };
   };
 
@@ -86,103 +117,97 @@ in {
     pkgs.unstable.ollama
   ];
 
+  # KDE Plasma Desktop Environment
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    kdepim-runtime
+    oxygen
+    konsole
+  ];
+
+  users.defaultUserShell = pkgs.zsh;
+
+  # User account
+  users.users = {
+    "williamj" = {
+      isNormalUser = true;
+      description = "William Jelgren";
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+        "docker"
+        "libvirtd"
+      ];
+      uid = 1000;
+    };
+  };
+
+  # Networking
+  networking = {
+    hostName = systemSettings.hostname;
+    networkmanager.enable = true;
+
+    # Firewall
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [
+        22 # OpenSSH
+        57621 # Spotify
+      ];
+      allowedUDPPorts = [
+        5353 # Spotify
+        4242 # lan-mouse
+      ];
+    };
+  };
+
   modules.system = {
     hardware.printing.enable = true;
     hardware.power.enable = false;
   };
 
-  # zsh
-  users.defaultUserShell = pkgs.zsh;
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    syntaxHighlighting.enable = true;
-    ohMyZsh = {
+  programs = {
+    zsh = {
       enable = true;
-      theme = "robbyrussell";
-      plugins = [
-        "history"
-        "rust"
-      ];
+      enableCompletion = true;
+      syntaxHighlighting.enable = true;
+      ohMyZsh = {
+        enable = true;
+        theme = "robbyrussell";
+        plugins = [
+          "history"
+          "rust"
+        ];
+      };
+    };
+
+    kdeconnect.enable = true;
+
+    # direnv
+    direnv.enable = true;
+    direnv.silent = false;
+
+    virt-manager.enable = true;
+
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
     };
   };
 
-  # direnv
-  programs.direnv.enable = true;
-  programs.direnv.silent = false;
+  virtualisation = {
+    libvirtd.enable = true;
+    spiceUSBRedirection.enable = true;
 
-  # VM's
-  programs.virt-manager.enable = true;
-  virtualisation.libvirtd.enable = true;
-  virtualisation.spiceUSBRedirection.enable = true;
-
-  # Docker
-  virtualisation.docker.enable = true;
-  virtualisation.docker.enableOnBoot = false;
-
-  services.timesyncd.enable = true;
-
-  # Steam
-  hardware.graphics.enable32Bit = true;
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
+    docker.enable = true;
+    docker.enableOnBoot = false;
   };
-
-  # Bluetooth
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # NVidia sucks
-  hardware.graphics.enable = true;
-  hardware.graphics.extraPackages = [pkgs.nvidia-vaapi-driver];
-  services.xserver.videoDrivers = ["nvidia"];
-  hardware.nvidia.open = true;
-
-  # OpenSSH
-  services.openssh = {
-    enable = true;
-    ports = [ 22 ];
-    settings = {
-      PasswordAuthentication = true;
-      AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
-      UseDns = true;
-      X11Forwarding = true;
-      PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
-    };
-  };
-
-  # KDE Plasma Desktop Environment
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-  environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    oxygen
-  ];
-
-  # KDE Connect
-  programs.kdeconnect.enable = true;
-
-  services.xserver.excludePackages = [pkgs.xterm];
-
-  # Configure keymap in X11
-  services.xserver.xkb.layout = "us";
 
   # Configure console keymap
   console.keyMap = "us";
 
-  # Enable pipewire for sound
-  hardware.pulseaudio.enable = false;
+  # Pipewire realtime security
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
 
   system.stateVersion = "24.11";
 }
