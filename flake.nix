84ced09da0d8530734a1990716de0f48109272e2
William Jelgren <william@jelgren.dk>
2026-01-05 14:23:09 +0100

    refac: Renamed some root folders

--- flake.nix
+++ flake.nix
@@ -21,75 +21,78 @@
     split-monitor-workspaces.url = "github:Duckonaut/split-monitor-workspaces";
     split-monitor-workspaces.inputs.hyprland.follows = "hyprland";
 
-		nixos-ddcci-nvidia.url = "github:poogas/nixos-ddcci-nvidia";
+    nixos-ddcci-nvidia.url = "github:poogas/nixos-ddcci-nvidia";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    alejandra,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      alejandra,
+      ...
+    }@inputs:
+    let
+      inherit (self) outputs;
 
-    # All supported systems
-    systems = [
-      "aarch64-linux"
-      "i686-linux"
-      "x86_64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
-    ];
+      # All supported systems
+      systems = [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
 
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-  in {
-    # Custom packages
-    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      # Custom packages
+      packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
 
-    # Formatter for nix files
-    formatter = forAllSystems (system: alejandra.defaultPackage.${system});
+      # Formatter for nix files
+      formatter = forAllSystems (system: alejandra.defaultPackage.${system});
 
-    overlays = import ./overlays {inherit inputs;};
+      overlays = import ./overlays { inherit inputs; };
 
-    nixosModules = import ./modules/nixos;
-    homeManagerModules = import ./modules/home-manager;
+      nixosModules = import ./modules/nixos;
+      homeManagerModules = import ./modules/home-manager;
 
-    # NixOS config entrypoint
-    nixosConfigurations = {
-      "little" = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          ./nixos/little
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users."williamj" = import ./home-manager/williamj;
+      # NixOS config entrypoint
+      nixosConfigurations = {
+        "little" = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            ./machines/little
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users."williamj" = import ./homes/williamj;
 
-              extraSpecialArgs = {inherit inputs outputs;};
-            };
-          }
-        ];
-      };
+                extraSpecialArgs = { inherit inputs outputs; };
+              };
+            }
+          ];
+        };
 
-      "emperor" = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          ./nixos/emperor
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users."williamj" = import ./home-manager/williamj;
+        "emperor" = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            ./machines/emperor
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users."williamj" = import ./homes/williamj;
 
-              extraSpecialArgs = {inherit inputs outputs;};
-            };
-          }
-        ];
+                extraSpecialArgs = { inherit inputs outputs; };
+              };
+            }
+          ];
+        };
       };
     };
-  };
 }
